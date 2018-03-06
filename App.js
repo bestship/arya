495e8999d06827d4a96e8168e7fd885bed806a77
bestship <37078055+bestship@users.noreply.github.com>
2018-03-06 11:20:49 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,93 @@
+//import React from 'react';
+//import { StyleSheet, Text, View } from 'react-native';
+
+//export default class App extends React.Component {
+//  render() {
+  //  return (
+  //    <View style={styles.container}>
+//        <Text>Open up App.js to start working on your app!</Text>
+//        <Text>Changes you make will automatically reload.</Text>
+//        <Text>Shake your phone to open the developer menu.</Text>
+//      </View>
+//    );
+//  }
+//}
+
+//const styles = StyleSheet.create({
+//  container: {
+//    flex: 1,
+//    backgroundColor: 'yellow',
+//    alignItems: 'center',
+//    justifyContent: 'center',
+//  },
+//});
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Button,
+  Text, TextInput,
+  View
+} from 'react-native';
+
+export default class HitungVolumeBalok extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      panjang:0,
+      tinggi:0,
+      lebar:0,
+      volume:0
+    };
+  }
+
+  render() {
+    return (
+      <View style = {{flex:1,backgroundColor:'#bbdefb'}}>
+
+        <View style={{backgroundColor:'#2196f3'}}>
+           <Text style = {{padding: 10, fontSize: 20, color: 'white', textAlign:'center'}} >
+            Menghitung Volume Balok
+          </Text>
+         </View>
+
+        <View style={{margin:20,padding: 10, backgroundColor:'#e3f2fd'}}>
+            <TextInput style = {{height: 40}}
+              placeholder="Masukkan Panjang"
+              onChangeText={(panjang)=>this.setState({panjang})}
+              keyboardType = 'numeric'
+            />
+            <TextInput style = {{height: 40}}
+              placeholder="Masukkan  Lebar"
+              onChangeText={(lebar)=>this.setState({lebar})}
+              keyboardType = 'numeric'
+            />
+            <TextInput style = {{height: 40}}
+              placeholder="Masukkan  Tinggi"
+              onChangeText={(tinggi)=>this.setState({tinggi})}
+              keyboardType = 'numeric'
+            />
+
+            <Button
+              onPress={()=>this.setState({
+                volume: (this.state.panjang*this.state.lebar*this.state.tinggi)
+              })}
+              title="Proses"
+              accessibilityLabel="Klik untuk menghitung"
+            />
+       </View>
+
+        <View style={{margin:20, backgroundColor:'#90caf9'}}>
+          <Text style = {{padding: 10, fontSize: 20}} >
+              Panjang =  {this.state.panjang} {"\n"}
+              Lebar =  {this.state.lebar} {"\n"}
+              Tinggi =  {this.state.tinggi} {"\n"}
+              Volume = {this.state.volume}
+          </Text>
+         </View>
+   </View>
+    );
+  }
+}
+AppRegistry.registerComponent('AppForm2', () => HitungVolumeBalok);
